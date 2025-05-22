f69f692bcdb760202ed7bc3ceb513cc6be12f1e0
shorabh-songara <shorabhkvs@gmail.com>
2025-05-23 04:21:35 +0530

    add titleinput feature

---
+++ Frontend/resume-builder/src/components/Inputs/TitleInput.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { LuCheck , LuPencil} from "react-icons/lu";
+function TitleInput({title , setTitle}){
+    const [showInput , setShowInput] = useState(false);
+
+    return(
+        <div className="flex items-center gap-3">
+            {showInput ?(
+                <>
+                <input
+                type="text"
+                placeholder="Resume Title"
+                className="text-sm md:text-[17px] bg-transparent outline-none text-black font-semibold border-b border-gray-300 pb-1"
+                value={title}
+                onChange={(e)=>setTitle(e.target.value)}/>
+                <button className="cursor-pointer">
+                    <LuCheck
+                    className="text-[16px] text-purple-600"
+                    onClick={()=>setShowInput((prevState)=>!prevState)}/>
+
+                </button>
+                </>
+            ):(
+                <>
+                <h2  className="text-sm md:text-[17px] font-semibold  ">{title}</h2> 
+                <button className="cursor-pointer">
+                    <LuPencil
+                    className="text-sm text-purple-600 "
+                    onClick={()=>setShowInput((prevState)=>!prevState)}/>
+                </button>
+                </>
+            )}
+        </div>
+    )
+
+}
+export default TitleInput;
